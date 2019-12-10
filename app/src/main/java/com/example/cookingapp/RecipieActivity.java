0881f6eac01ace2208bdfe24825babe0035fa1c6
Samolberg <larssamolberg@gmail.com>
2019-12-10 19:09:02 +0200

    Ajud tehtud

---
+++ app/src/main/java/com/example/cookingapp/RecipieActivity.java
@@ -0,0 +1,82 @@
+package com.example.cookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RecipieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipie);
+
+        ArrayList<String> selected = getIntent().getStringArrayListExtra("selected");
+        Log.d("retsept", "length" + selected.size());
+        try {
+            JSONArray array = new JSONArray(loadJSONFromAsset(this));
+            int bestRecipeIndex = 0;
+            int bestRecipeMatches = 0;
+            for (int i = 0; i < array.length(); i++) {
+                int currentMatches = 0;
+                for (int j = 0; j < array.getJSONObject(i).getJSONArray("ingredients").length(); j++) {
+                    String ingredient = array.getJSONObject(i).getJSONArray("ingredients").getString(j);
+                  if(selected.contains(ingredient)){
+                      currentMatches++;
+                  }
+                }
+                if(currentMatches > bestRecipeMatches){
+                    bestRecipeMatches = currentMatches;
+                    bestRecipeIndex = i;
+                }
+            }
+
+              Log.d("retsept", "Best match "+bestRecipeIndex);
+              Log.d("retsept", "Best matches "+bestRecipeMatches);
+            JSONObject retsept = array.getJSONObject(bestRecipeIndex);
+            ImageView recipeImage = findViewById(R.id.recipeImage);
+            TextView recipeTutorial = findViewById(R.id.recipeTutorial);
+            recipeTutorial.setText(retsept.getString("tutorial"));
+            Picasso.with(this).load(retsept.getString("image")).into(recipeImage);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public String loadJSONFromAsset(Context context) {
+        String json = null;
+        try {
+            InputStream is = context.getAssets().open("Recipe.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return null;
+        }
+        return json;
+
+    }
+
+
+}
--- app/src/main/java/com/example/cookingapp/SecondActivity.java
+++ app/src/main/java/com/example/cookingapp/SecondActivity.java
@@ -2,7 +2,13 @@ package com.example.cookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Switch;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +16,19 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+    }
+
+    public void toRecipie (View view){
+       ArrayList<String> selected = new ArrayList<>();
+        Switch chickenSwitch = findViewById(R.id.chickenSwitch);
+        Switch meatSwitch = findViewById(R.id.meatSwitch);
+        if (chickenSwitch.isChecked()){
+            selected.add("chicken");
+        }if (meatSwitch.isChecked()){
+            selected.add("meat");
+        }
+        Intent recipieIntent = new Intent (this, RecipieActivity.class);
+        recipieIntent.putStringArrayListExtra("selected",selected);
+        startActivity(recipieIntent);
     }
 }
